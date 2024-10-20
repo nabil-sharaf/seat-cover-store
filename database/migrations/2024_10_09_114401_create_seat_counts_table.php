4b698ecac621e1c4d4e435c1225a983f9dd4441e
nabil abusharaf <nabil.sharaf88@gmail.com>
2024-10-20 21:55:24 +0300

    seeding

--- database/migrations/2024_10_09_114401_create_seat_counts_table.php
+++ database/migrations/2024_10_09_114401_create_seat_counts_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,6 +12,14 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // تعطيل التحقق من المفاتيح الأجنبية
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        DB::table('seat_counts')->truncate();
+
+// إعادة تمكين التحقق من المفاتيح الأجنبية
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
         Schema::create('seat_counts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
